47fb6e45a8efa89efc910e4862e2cb2fd9dd767e
bunchmj <matt.bunch@defenseunicorns.com>
2024-01-09 18:36:06 -0600

    fix dorked files

--- complete/output.tf
+++
@@ -1,3 +0,0 @@
-output "reserved_ips_per_subnet" {
-  value = module.vpc.reserved_ips_per_subnet
-}
--- test/e2e/examples_complete_insecure_test.go
+++ test/e2e/examples_complete_insecure_test.go
@@ -1,27 +1,24 @@
 package e2e_test
 
 import (
-	"os"
 	"testing"
 	"time"
 
-	"github.com/defenseunicorns/delivery_aws_iac_utils/pkg/utils"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	teststructure "github.com/gruntwork-io/terratest/modules/test-structure"
+
+	"github.com/defenseunicorns/delivery_aws_iac_utils/pkg/utils"
 )
 
-func TestExamplesCompleteCommon(t *testing.T) {
+func TestExamplesCompleteInsecure(t *testing.T) {
 	t.Parallel()
-
-	// Set the TF_VAR_region to us-east-2 if it's not already set
-	utils.SetDefaultEnvVar("TF_VAR_region", "us-east-2")
-
 	tempFolder := teststructure.CopyTerraformFolderToTemp(t, "../..", "examples/complete")
 	terraformOptions := &terraform.Options{
 		TerraformDir: tempFolder,
 		Upgrade:      false,
-		EnvVars: map[string]string{
-			"TF_VAR_region": os.Getenv("TF_VAR_region"), // This will use the existing or newly set default value
+		VarFiles: []string{
+			"fixtures.common.tfvars",
+			"fixtures.insecure.tfvars",
 		},
 		RetryableTerraformErrors: map[string]string{
 			".*": "Failed to apply Terraform configuration due to an error.",
@@ -45,6 +42,9 @@ func TestExamplesCompleteCommon(t *testing.T) {
 
 	// Run assertions
 	teststructure.RunTestStage(t, "TEST", func() {
-		// Assertions go here
+		utils.ValidateEFSFunctionality(t, tempFolder)
+		utils.DownloadZarfInitPackage(t)
+		utils.ConfigureKubeconfig(t, tempFolder)
+		utils.ValidateZarfInit(t, tempFolder)
 	})
 }
