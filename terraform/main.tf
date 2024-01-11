eb96ad4e2d3383e3c81fa40a4450c0238c1ccfac
bunchmj <matt.bunch@defenseunicorns.com>
2024-01-10 21:00:56 -0600

    setup iac, wip

---
+++ terraform/main.tf
@@ -0,0 +1,40 @@
+data "aws_partition" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+data "aws_ami" "eks_default_bottlerocket" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["bottlerocket-aws-k8s-${var.cluster_version}-x86_64-*"]
+  }
+}
+
+resource "random_id" "default" {
+  byte_length = 2
+}
+
+locals {
+  vpc_name                   = "${var.name_prefix}-${lower(random_id.default.hex)}"
+  cluster_name               = "${var.name_prefix}-${lower(random_id.default.hex)}"
+  bastion_name               = "${var.name_prefix}-bastion-${lower(random_id.default.hex)}"
+  access_logging_name_prefix = "${var.name_prefix}-accesslog-${lower(random_id.default.hex)}"
+  kms_key_alias_name_prefix  = "alias/${var.name_prefix}-${lower(random_id.default.hex)}"
+  access_log_sqs_queue_name  = "${var.name_prefix}-accesslog-access-${lower(random_id.default.hex)}"
+  tags = merge(
+    var.tags,
+    {
+      RootTFModule = replace(basename(path.cwd), "_", "-") # tag names based on the directory name
+      GithubRepo   = "github.com/defenseunicorns/delivery-aws-iac"
+    }
+  )
+}
