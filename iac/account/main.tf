3ea3cdcd859065c75ba30f222707140c25cc2627
bunchmj <matt.bunch@defenseunicorns.com>
2024-02-07 15:23:07 -0600

    cleanup

--- iac/account/main.tf
+++ iac/account/main.tf
@@ -2,39 +2,23 @@ provider "aws" {
   region = var.region
 }
 
-#S3 access controls, policies and logging should be created as seperate terraform resources
-#tfsec:ignore:aws-s3-block-public-acls tfsec:ignore:aws-s3-block-public-policy tfsec:ignore:aws-s3-enable-bucket-encryption tfsec:ignore:aws-s3-encryption-customer-key tfsec:ignore:aws-s3-ignore-public-acls tfsec:ignore:aws-s3-no-public-buckets tfsec:ignore:aws-s3-enable-bucket-logging tfsec:ignore:aws-s3-enable-versioning tfsec:ignore:aws-s3-specify-public-access-block
-# resource "aws_s3_bucket" "default" {
-#   count = module.this.enabled ? 1 : 0
-
-#   bucket = "${module.this.id}-logs"
-# }
-
 module "tfstate_backend" {
-  source = "cloudposse/tfstate-backend/aws"
+  source                             = "cloudposse/tfstate-backend/aws"
+  version                            = "1.4.0"
+  terraform_backend_config_file_path = var.create_local_backend_file ? "." : ""
+  terraform_backend_config_file_name = var.create_local_backend_file ? "backend.tf" : ""
+  arn_format                         = var.arn_format
+  s3_bucket_name                     = var.s3_bucket_name
 
-  bucket_enabled   = var.bucket_enabled
-  dynamodb_enabled = var.dynamodb_enabled
-
-  # logging = [
-  #   {
-  #     target_bucket = one(aws_s3_bucket.default[*].id)
-  #     target_prefix = "tfstate/"
-  #   }
-  # ]
-
-  bucket_ownership_enforced_enabled = true
-
-  #context = module.this.context
-  version    = "1.4.0"
   namespace  = "du"
   stage      = "test"
   name       = "narwhal-delivery-iac-swf"
   attributes = ["state"]
-  arn_format = "arn:aws-us-gov"
 
-  terraform_backend_config_file_path = "."
-  terraform_backend_config_file_name = "backend.tf"
-  force_destroy                      = false
-  #enabled = false
+  tags = var.tags
+
+  bucket_enabled                    = var.bucket_enabled
+  dynamodb_enabled                  = var.dynamodb_enabled
+  bucket_ownership_enforced_enabled = var.bucket_ownership_enforced_enabled
+  force_destroy                     = var.force_destroy
 }
